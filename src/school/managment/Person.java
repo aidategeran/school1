4fdb2ddc8ffe59dc5ea8ab9fd90c7dbefe901ec6
AidaTegeran <aidategeran@gmail.com>
2024-10-11 22:17:40 -0500

    school1

--- src/school/managment/Person.java
+++ src/school/managment/Person.java
@@ -1,4 +1,17 @@
 package school.managment;
 
 public class Person {
+    public String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
 }
+
--- src/school/managment/Student.java
+++ src/school/managment/Student.java
@@ -1,4 +1,16 @@
 package school.managment;
 
-public class Student {
+public class Student extends Person {
+    private int studentId;
+    private String [] enrolledCourses;
+    private double tuition;
+
+    public Student(String name, int age, int studentId) {
+        super(name, age);
+        this.studentId = studentId;
+
+    }
+
+    public void enrollCourses()
+
 }
